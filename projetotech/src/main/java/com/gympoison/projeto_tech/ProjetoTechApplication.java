d0e4c320dd7890995d18daac5c373143c4fed474
koda012 <lucaskojimaeda@gmail.com>
2024-11-27 18:35:42 -0300

    att

--- projetotech/src/main/java/com/gympoison/projeto_tech/ProjetoTechApplication.java
+++ projetotech/src/main/java/com/gympoison/projeto_tech/ProjetoTechApplication.java
@@ -3,7 +3,7 @@ package com.gympoison.projeto_tech;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication;
 public class ProjetoTechApplication {
 
 	public static void main(String[] args) {
