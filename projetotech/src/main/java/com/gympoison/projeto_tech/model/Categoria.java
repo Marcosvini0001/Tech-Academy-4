26c967ffe1c1f8847735e83bc4c4f8518d07e16d
Marcos Vinicius Bartoli Senko <mvsenko7@gmail.com>
2024-11-30 21:30:23 -0300

    feito algumas alteracoes em usuario e categora

--- projetotech/src/main/java/com/gympoison/projeto_tech/model/Categoria.java
+++ projetotech/src/main/java/com/gympoison/projeto_tech/model/Categoria.java
@@ -11,4 +11,20 @@ public class Categoria {
 
     @Column
     private Integer id;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
