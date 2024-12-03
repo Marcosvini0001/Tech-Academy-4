343d65ddb09f125faaeda1545d41275409f37083
koda012 <lucaskojimaeda@gmail.com>
2024-12-02 22:17:28 -0300

    atualização

--- projetotech/src/main/java/com/gympoison/projeto_tech/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gympoison.projeto_tech.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<CategoriaRepository, Integer> {
-}
